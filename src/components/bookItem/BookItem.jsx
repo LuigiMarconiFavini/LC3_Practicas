86dfa72be98a61c82aeaa452322b419fd15b6f18
LuigiMarconiFavini <luigimarconifavini@gmail.com>
2024-04-03 17:07:01 -0300

    proyectos

---
+++ src/components/bookItem/BookItem.jsx
@@ -0,0 +1,23 @@
+import "./BookItem.css";
+import PropTypes from 'prop-types';
+
+const BookItem = ({ title, author, pageCount, rating}) => {
+
+  return (
+    <div>
+        <h2>{title}</h2>
+        <h3>{author}</h3>
+        <div>{rating.length}</div>
+        <p>{pageCount} páginas</p>
+    </div>
+  )
+}
+
+BookItem.propTypes = {
+    title: PropTypes.string,
+    author: PropTypes.string,
+    pageCount: PropTypes.number,
+    rating: PropTypes.array,
+};
+
+export default BookItem
